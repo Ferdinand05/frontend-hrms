519a57a75b4fbf326fe6c4601793c20804e15084
Ferdinand05 <fkoryanto@gmail.com>
2025-10-18 21:21:08 +0700

    create view for salary

---
+++ src/views/admin/salary/SalaryView.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import type { breadcrumbItem } from "@/types/breadcrumb";
+import DashboardLayout from "@/views/layouts/DashboardLayout.vue";
+
+const breadcrumbs: breadcrumbItem[] = [
+  {
+    name: "Employee Salary",
+    href: "/admin/salaries",
+  },
+];
+</script>
+
+<template>
+  <DashboardLayout
+    :breadcrumbs="breadcrumbs"
+    heading="Salaries Management"
+    subheading="Manage all employee salary here"
+  >
+  </DashboardLayout>
+</template>
