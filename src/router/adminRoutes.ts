6ee5a0661039fc7ca017c74ed9bd154eb4adc7b5
Ferdinand05 <fkoryanto@gmail.com>
2025-10-18 21:21:44 +0700

    library, routes and types

--- src/router/adminRoutes.ts
+++ src/router/adminRoutes.ts
@@ -2,10 +2,13 @@ import AttendanceView from "@/views/admin/attendance/AttendanceView.vue";
 import DashboardView from "@/views/admin/DashboardView.vue";
 import DepartmentView from "@/views/admin/department/DepartmentView.vue";
 import CreateEmployee from "@/views/admin/employee/CreateEmployee.vue";
+import EditEmployee from "@/views/admin/employee/EditEmployee.vue";
 import EmployeeView from "@/views/admin/employee/EmployeeView.vue";
 import LeaveView from "@/views/admin/leave/LeaveView.vue";
 import RoleView from "@/views/admin/role/RoleView.vue";
+import SalaryView from "@/views/admin/salary/SalaryView.vue";
 import CreateUser from "@/views/admin/user/CreateUser.vue";
+import EditUser from "@/views/admin/user/EditUser.vue";
 import UserView from "@/views/admin/user/UserView.vue";
 import type { RouteRecordRaw } from "vue-router";
 
@@ -40,6 +43,12 @@ export const adminRoutes: RouteRecordRaw = {
           component: CreateUser,
           meta: { title: "Create User", portal: "admin" },
         },
+        {
+          path: ":id/edit",
+          name: "edit-user",
+          component: EditUser,
+          meta: { title: "Edit User", portal: "admin" },
+        },
       ],
     },
     // admin role management route
@@ -70,6 +79,12 @@ export const adminRoutes: RouteRecordRaw = {
           component: CreateEmployee,
           meta: { title: "Create Employee", portal: "admin" },
         },
+        {
+          path: ":id/edit",
+          name: "edit-employee",
+          component: EditEmployee,
+          meta: { title: "Edit Employee", portal: "admin" },
+        },
       ],
     },
     {
@@ -90,6 +105,12 @@ export const adminRoutes: RouteRecordRaw = {
       name: "payrolls",
       component: () => import("@/views/admin/payroll/PayrollView.vue"),
       meta: { title: "Payroll Management", portal: "admin" },
+    },
+    {
+      path: "salaries",
+      name: "salaries-list",
+      component: SalaryView,
+      meta: { title: "Salary Management", portal: "admin" },
     },
   ],
 };
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,48 @@
+import type { EmployeeType } from "./user";
+
 export type RoleType = {
   id: number;
   role_name: string;
   created_at: string;
   count: number;
+};
+
+export type Department = {
+  id: number;
+  name: string;
+  description: string;
+  count: number;
+  created_at: string;
+};
+
+export type Office = {
+  id: number;
+  app_name: string;
+  latitude: number;
+  longitude: number;
+  radius: number;
+  max_accuracy: number;
+};
+
+export type Attendance = {
+  id: number;
+  employee_id: number;
+  employee: EmployeeType;
+  date: string;
+  clock_in: string;
+  clock_out: string | null;
+  image_path: string;
+  latitude: number;
+  longitude: number;
+  accuracy: number;
+  status: string;
+  created_at: string;
+};
+
+type MenuItem = {
+  name: string;
+  color: string;
+  icon: unknown;
+  function?: () => void;
+  disabled?: boolean;
 };
